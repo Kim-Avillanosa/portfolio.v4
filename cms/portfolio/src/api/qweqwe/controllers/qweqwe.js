1185df04fcfd2aae27be9c8f405de842c4a93bf7
Kim Avillanosa <careers.kmavillanosa@gmail.com>
2024-07-05 07:33:02 +0000

    modified details

--- cms/portfolio/src/api/qweqwe/controllers/qweqwe.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * qweqwe controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::qweqwe.qweqwe');
