9c3c90bab6ab571bc21d2a06b4d995ba5d878a5b
Kim Avillanosa <careers.kmavillanosa@gmail.com>
2024-07-06 15:36:05 +0000

    added frontend files

---
+++ app/portfolio/src/common/components/Page.tsx
@@ -0,0 +1,17 @@
+import { Head } from "next/document";
+import { PropsWithChildren } from "react";
+
+interface Props {
+  title: string;
+}
+
+const Page: React.FC<PropsWithChildren<Props>> = ({ children, title }) => {
+  return (
+    <>
+    <title>{title}</title>
+      {children}
+    </>
+  );
+};
+
+export default Page;
